c690d94fe39be33a2a566b5d3034a7a5b4663cea
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-06 04:12:07 +0530

    Create MemberRepository.java

---
+++ src/main/java/com/jason/foody/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.jason.foody.repository;
+
+import com.jason.foody.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+}
