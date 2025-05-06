e737803349c4d14b0aeb9b9da9f715f9324dd40e
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-06 19:21:25 +0530

    Update Member.java

--- src/main/java/com/jason/foody/entity/Member.java
+++ src/main/java/com/jason/foody/entity/Member.java
@@ -27,6 +27,48 @@ public class Member {
     LocalDateTime created;
     LocalDateTime modified;
 
+    public UUID getId() {
+        return id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getModified() {
+        return modified;
+    }
+
+    public void setModified(LocalDateTime modified) {
+        this.modified = modified;
+    }
 }
 
