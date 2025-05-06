81e57018e7253bd8bea626f4bbe157c80078c124
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-06 19:22:55 +0530

    Update MemberService.java

--- src/main/java/com/jason/foody/service/MemberService.java
+++ src/main/java/com/jason/foody/service/MemberService.java
@@ -1,10 +1,13 @@
 package com.jason.foody.service;
 
 import com.jason.foody.entity.Member;
+import com.jason.foody.exception.InvalidIdException;
+import com.jason.foody.exception.InvalidOperationException;
 import com.jason.foody.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,8 +16,33 @@ public class MemberService {
     @Autowired
     private MemberRepository memberRepository;
 
+    public UUID saveMember(Member member){
+        member.setCreated(LocalDateTime.now());
+        Member savedMember = memberRepository.save(member);
+        return savedMember.getId();
+    }
+
     public List<Member> getAllMembers(){
         return memberRepository.findAll();
+    }
+
+    public Member getMemberById(UUID id) throws InvalidIdException {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new InvalidIdException("There is no member with id: " + id));
+    }
+
+    public void updateMember(Member member) throws InvalidIdException, InvalidOperationException {
+        Member savedMember = getMemberById(member.getId());
+        if(!savedMember.getEmail().equals(member.getEmail())){
+            throw new InvalidOperationException("Email id cannot be modified");
+        }
+        savedMember.setModified(LocalDateTime.now());
+        memberRepository.save(member);
+    }
+
+    public void removeMember(Member member) throws InvalidIdException {
+        Member savedMember = getMemberById(member.getId());
+        memberRepository.delete(savedMember);
     }
 
 }
