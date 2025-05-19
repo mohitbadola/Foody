c63a7e19446901758db176be963ac97708f10d78
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-20 00:55:56 +0530

    Create MyMemberDetails.java

---
+++ src/main/java/com/jason/foody/security/MyMemberDetails.java
@@ -0,0 +1,56 @@
+package com.jason.foody.security;
+
+import com.jason.foody.entity.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class MyMemberDetails implements UserDetails {
+
+    private Member member;
+
+    public MyMemberDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        member.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRole())));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
