6e2cebe35e10200fd955f7c9783fce70e5730be4
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-20 00:58:36 +0530

    implement SecurityFilterChain

--- src/main/java/com/jason/foody/security/SecurityConfig.java
+++ src/main/java/com/jason/foody/security/SecurityConfig.java
@@ -1,16 +1,36 @@
 package com.jason.foody.security;
 
+import com.jason.foody.service.MemberDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private MemberDetailsServiceImpl memberDetailsService;
+
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.authorizeHttpRequests(auth->
+                auth.requestMatchers("food-item/add").authenticated()
+                        .anyRequest().permitAll()
+        ).httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .userDetailsService(memberDetailsService)
+                .build();
     }
 }
