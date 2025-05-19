48c9a76f92e086fcb0bc33ccb75e82c45fe11f11
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-19 18:27:12 +0530

    Create RoleRepository.java

---
+++ src/main/java/com/jason/foody/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.jason.foody.repository;
+
+import com.jason.foody.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
