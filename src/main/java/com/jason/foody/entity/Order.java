bc0ab540256a8332545efce97c54573ade88c87d
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-08-09 21:19:29 +0530

    Update Order.java

--- src/main/java/com/jason/foody/entity/Order.java
+++ src/main/java/com/jason/foody/entity/Order.java
@@ -1,11 +1,9 @@
 package com.jason.foody.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity(name = "orders")
 public class Order {
@@ -17,4 +15,39 @@ public class Order {
     private Double amount;
 
     private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
